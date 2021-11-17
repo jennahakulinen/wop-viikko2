2059ccb941ba9c3d2772d83977d01d8a88b4e884
jennahakulinen <jenna.hakulinen@metropolia.fi>
2021-11-17 11:15:36 +0200

    week4 commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
+const authRoute = require('./routes/authRoute');
+const passport = require('./utils/pass');
 const { httpError } = require('./utils/errors');
 
 const app = express();
@@ -16,8 +18,11 @@ app.use(cors());
 
 app.use(express.static('./uploads/'));
 
-app.use('/cat', catRoute);
-app.use('/user', userRoute);
+app.use(passport.initialize());
+
+app.use('/auth', authRoute);
+app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
+app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 
 app.use((req, res, next) => {
     const err = httpError('Not found', 404);
